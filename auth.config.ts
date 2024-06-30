7ccb8a3d4d93276750d7b0ff7d848d9853b0c2ee
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2024-06-30 23:25:37 +0545

    oauth with github and google

--- auth.config.ts
+++ auth.config.ts
@@ -3,10 +3,20 @@ import type { NextAuthConfig } from "next-auth";
 import { loginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
 import bcrypt from "bcrypt";
+import github from "next-auth/providers/github";
+import google from "next-auth/providers/google";
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [
+    google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = loginSchema.safeParse(credentials);
--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth, { DefaultSession } from "next-auth";
 import authConfig from "./auth.config";
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { getUserById } from "./data/user";
 
@@ -16,12 +16,17 @@ const prisma = new PrismaClient();
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   callbacks: {
+    async signIn({ user }) {
+      const existingUser = await getUserById(user.id);
+      if (!existingUser || !existingUser.emailVerified) return false;
+      return true;
+    },
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
       if (token.role && session.user) {
-        session.user.role = token.role;
+        session.user.role = token.role as UserRole;
       }
       console.log({ sessiontoken: token });
       return session;
